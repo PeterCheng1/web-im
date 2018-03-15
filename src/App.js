d1e0519d696d679a028ae15562a5585b0e0044b1
PeterCheng1 <1284995325@qq.com>
2018-03-15 14:32:05 +0800

    set router

--- src/App.js
+++ src/App.js
@@ -2,27 +2,29 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import LoginPage from './containers/login/index'
+import ChatPage from './containers/chat/chat'
 import {Provider} from 'react-redux';
 import store from './data/create'
 import {safeRender} from '@assets/js/safeRender'
-
-@safeRender
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <LoginPage />
-      </div>
-    );
-  }
-}
-
+import { Router, Route, Link,hashHistory } from 'react-router'
+/*
+  路由:
+  / ==> 登陆注册页面
+  /chat ==> 聊天页面
+  /chat/single/:uid ==> 单聊
+  /chat/group/:uid ==> 群聊
+*/
 @safeRender
 class Main extends Component {
   render() {
     return (
       <Provider store={store}>
-        <App/>
+        <Router history={hashHistory} >
+          <Route path="/" component={LoginPage} />
+          <Route path="/chat" component={ChatPage} />
+          <Route path="/chat/single/:uid" component={ChatPage} />
+          <Route path="/chat/group/:uid" component={ChatPage} />
+        </Router>
       </Provider>
     )
   }
---
+++ src/containers/chat/chat.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Chat extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div className="chat">
+                Chat
+            </div>
+        )
+    }
+}
+
+
+export default Chat;
