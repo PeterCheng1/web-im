11b2eac1607f2c8822a4db420719e20f23d4561a
PeterCheng1 <1284995325@qq.com>
2018-04-27 17:49:53 +0800

    .

--- src/data/reducers/noReadMsgReducer.js
+++ src/data/reducers/noReadMsgReducer.js
@@ -20,3 +20,23 @@ export default function noReadMsgReducer(state = initialState,action){
     }
 }
 
+/**
+ * new的特性
+ * new 的结果是一个新对象
+ * 结果会具有构造函数里的属性
+ * 我们知道实例的 __proto__ 属性会指向构造函数的 prototype，也正是因为建立起这样的关系，实例可以访问原型上的属性
+ *  objectFactory(Otaku, ……) 调用
+*/
+
+const objectFactory = function() {
+    let obj = new Object();
+    let Constructor = [].shift.call(arguments);
+    obj.Constructor = Constructor;
+    obj.__proto__ = Constructor.prototype;
+    const ret = Constructor.apply(obj,Array.from(arguments));
+    return typeof ret === 'object' ? ret : obj;
+}
+
+
+
+
